65c31b9107d64d9f849c16e9c7d124190d88a3d6
Juan Pablo Orjuela <juan.orjuelar@usantoto.edu.co>
2023-04-02 17:17:41 -0500

    Primera subida

---
+++ src/main/java/com/usta/opticavisionintegral/OpticaVisionIntegralApplication.java
@@ -0,0 +1,13 @@
+package com.usta.opticavisionintegral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticaVisionIntegralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpticaVisionIntegralApplication.class, args);
+    }
+
+}
